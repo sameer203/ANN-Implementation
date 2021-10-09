f1607d30be1f5c39c318b0589ec19ac3177e4b96
sameer203 <mdsameerajuvath.com>
2021-10-10 00:50:08 +0530

    get data function implemented

--- src/training.py
+++ src/training.py
@@ -1,18 +1,22 @@
 import os
 from src.utils.common import read_config
+from src.utils.data_mgmt import get_data
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
 
-    print(config)
+    validation_datasize = config["params"]["validation_datasize"]
+    (X_train, y_train),(X_valid, y_valid),(X_test, y_test) = get_data(validation_datasize)
+
+    #print(config)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config.yaml")
 
-    parsed_args = args.parsed_args()
+    parsed_args = args.parse_args()
 
     training(config_path=parsed_args.config)
 
